22ae51b1e3cb8213ef040c1e1f665ee5a64ee5f8
ahadb802 <abdulahadb802@gmail.com>
2022-08-06 18:53:38 +0500

    initial commit 3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>My name is Abdul Ahad Bhatti</p>
+        <p>My name is Zaid</p>
         <a
           className="App-link"
           href="https://reactjs.org"
