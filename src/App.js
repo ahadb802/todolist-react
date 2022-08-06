d474e6448ea3670e68b4f3dad250c8fb93face3f
ahadb802 <abdulahadb802@gmail.com>
2022-08-06 19:02:02 +0500

    girl

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>My name is Zaid</p>
+        <p>I am a girl.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
